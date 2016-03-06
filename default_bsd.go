1b24af831e2d18ab763e9b723e679ff75c5383c9
Moriyoshi Koizumi <mozo@mozo.jp>
2016-03-07 02:02:07 +0900

    Add default factories.

---
+++ default_bsd.go
@@ -0,0 +1,20 @@
+// +build darwin,freebsd
+
+package routewrapper
+
+import (
+	"os/exec"
+)
+
+
+func NewRouteWrapper() (Routing, error) {
+	pathToNetstatCommand, err := exec.LookPath("netstat")
+	if err != nil {
+		return nil, err
+	}
+	pathToRouteCommand, err := exec.LookPath("route")
+	if err != nil {
+		return nil, err
+	}
+	return NewBSDRouteWrapper(pathToNetstatCommand, pathToRouteCommand)
+}
---
+++ default_windows.go
@@ -0,0 +1,16 @@
+// +build windows
+
+package routewrapper
+
+import (
+	"os/exec"
+)
+
+func NewRouteWrapper() (Routing, error) {
+	pathToRouteCommand, err := exec.LookPath("route")
+	if err != nil {
+		return nil, err
+	}
+	return NewWindowsRouteWrapper(pathToRouteCommand)
+}
+
